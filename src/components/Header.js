86bf2b0bdfcffa7774dd3d22f21e75c9e0ea5b1c
wolfitis <webblog123@gmail.com>
2021-08-19 09:47:37 -0400

    Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import Button from './Button'
 
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Clicked')
+  }
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='add' />
+      <Button color='green' text='add' onClick={onClick} />
     </header>
   )
 }
