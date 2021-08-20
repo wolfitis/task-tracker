b31cb9203b9eeda118d4fd15f62f05c8613f09f0
wolfitis <webblog123@gmail.com>
2021-08-20 00:36:43 -0400

    add button toggle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -53,8 +54,8 @@ function App() {
   return (
     <div className="container">
       <h1>Hello from React</h1>
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0
         ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
         : ('No Tasks to show')}
